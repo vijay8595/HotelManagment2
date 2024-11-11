174c9d3483f1cfddbb9083ef79fc43ab56ac092a
vijaykumar <136881396+vijay8595@users.noreply.github.com>
2024-11-11 23:38:53 +0530

    Update BookingResponseDto.java
    change BookingRequestDto  file

--- src/main/java/com/oyo/HotelManagment2/dto/request/BookingResponseDto.java
+++ src/main/java/com/oyo/HotelManagment2/dto/request/BookingResponseDto.java
@@ -1,4 +1,16 @@
 package com.oyo.HotelManagment2.dto.request;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookingResponseDto {
+  Integer customerId;
+  Integer hotelId;
+  Integer roomId;
+  LocalDate checkIn;
+  LocalDate checkOut;
+  Integer numberOfGuest;
+  Integer bookingAccount;
 }
